dd52c6e7621b4796b2965fcac8ef8a00c37f5d1c
Lucas <a.lucasd@gmail.com>
2023-09-21 15:12:20 -0300

    create and show new product

--- scripts.js
+++ scripts.js
@@ -1,23 +1,45 @@
 $(document).ready(function () {
     $("#mostrar").click(function (evento) {
         evento.preventDefault();
-        $(".prod").remove();
+        mostrar();
+    });
+
+    $("#agregar").click(function (evento) {
+        $("#error").html("");
+        evento.preventDefault();
+        let nombre = $("#nombre").val();
+        let precio = $("#precio").val();
         $.ajax({
-            type: 'GET',
-            url: "http://localhost:3000/public/api/products?perpage=100",
+            type: 'POST',
+            headers: { 'Accept': 'application/json' },
+            url: "http://localhost:3000/public/api/products",
+            data: { "name": nombre, "price": precio }
         }).done(function( msg ) {
-            msg.data.forEach(
-                producto =>
-                    $("#table").append(
-                        "<tr>" +
-                        "<td class='col-lg-4 prod'>"+producto['name']+"</td>" +
-                        "<td class='col-lg-4 prod'>"+producto['price']+"</td>" +
-                        "<td class='col-lg-4 prod'>"+producto['usd_price']+"</td>" +
-                        "</tr>"
-                    )
-            );
+            mostrar();
         }).fail(function (jqXHR, textStatus, errorThrown){
             $("#error").html("The following error occured: "+ textStatus +" "+ errorThrown);
         });
     });
 });
+
+function mostrar(){
+    $(".prod").remove();
+    $.ajax({
+        type: 'GET',
+        url: "http://localhost:3000/public/api/products?perpage=100",
+    }).done(function( msg ) {
+        msg.data.forEach(
+            producto =>
+                $("#table").append(
+                    "<tr>" +
+                    "<td class='col-lg-4 prod' style='border:1px solid black;'>"+producto['id   ']+"</td>" +
+                    "<td class='col-lg-4 prod' style='border:1px solid black;'>"+producto['name']+"</td>" +
+                    "<td class='col-lg-4 prod' style='border:1px solid black;'>"+producto['price']+"</td>" +
+                    "<td class='col-lg-4 prod' style='border:1px solid black;'>"+producto['usd_price']+"</td>" +
+                    "</tr>"
+                )
+        );
+    }).fail(function (jqXHR, textStatus, errorThrown){
+        $("#error").html("The following error occured: "+ textStatus +" "+ errorThrown);
+    });
+}
